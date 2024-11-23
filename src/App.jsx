fadd78ec143f554cfa7efa55074fb98cc555f864
naufalluth <naufaldvf123@gmail.com>
2024-11-23 20:26:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="" >
+      <h1 className='text-emerald-500 text-5xl text-center'>This is React + tailwindcss Boilerplate</h1>
+    </div >
+  )
+}
+
+export default App
